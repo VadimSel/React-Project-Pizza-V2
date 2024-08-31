12082d6ecb671b5abd2d7fa6725ce95142729de1
VadimSel <selyaaa33@gmail.com>
2024-08-31 16:03:28 +0300

    fixed the empty search bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
-import { useState } from "react";
+import { createContext, useState } from "react";
+
+export const SearchContext = createContext();
 
 function App() {
     const [searchValue, setSearchValue] = useState("");
 
     return (
         <div className="wrapper">
-            <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-                {/* <NotFound/> */}
-            </div>
+            <SearchContext.Provider value={{searchValue, setSearchValue}}>
+                <Header />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+            </SearchContext.Provider>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
+import { SearchContext } from "../App";
 import { Categories } from "../components/Categories";
+import Pagination from "../components/Pagination";
 import { PizzaBlock } from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { Sort } from "../components/Sort";
 
 const Home = () => {
+    const { searchValue } = useContext(SearchContext);
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [categoryId, setCategoryId] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [sortType, setSortType] = useState({
         name: "популярности",
         sortProperty: "rating",
@@ -20,9 +24,10 @@ const Home = () => {
         const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
         const sortBy = sortType.sortProperty.replace("-", "");
         const category = categoryId > 0 ? `category=${categoryId}` : "";
+        const search = searchValue ? `&search=${searchValue}` : "";
 
         fetch(
-            `https://66c9fd0759f4350f064e1891.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+            `https://66c9fd0759f4350f064e1891.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
         )
             .then((res) => {
                 return res.json();
@@ -32,7 +37,11 @@ const Home = () => {
                 setIsLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType]);
+    }, [categoryId, sortType, searchValue, currentPage]);
+
+    // const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+    const pizzas = Array.isArray(items) ? items.map((obj) => <PizzaBlock key={obj.id} {...obj} />) : [];
+    const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
     return (
         <div className="container">
@@ -44,11 +53,8 @@ const Home = () => {
                 <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
             </div>
             <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {isLoading
-                    ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                    : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-            </div>
+            <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+            <Pagination onChangePage={(number) => setCurrentPage(number)} />
         </div>
     );
 };
