24aaceafdc10769d541d99c1658d88576448126b
VadimSel <selyaaa33@gmail.com>
2024-09-07 16:53:07 +0300

    code splitting, lazy loading, name for chunks (27 is done)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Header from "./components/Header";
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
-import FullPizza from "./pages/FullPizza";
 
-import "./scss/app.scss";
-import { createContext, useState } from "react";
+import { lazy, Suspense } from "react";
 import MainLayout from "./layouts/MainLayout";
+import "./scss/app.scss";
+
+const Cart = lazy(() => import( /* webpackChunkName: "Cart" */ './pages/Cart'))
+const FullPizza = lazy(() => import( /* webpackChunkName: "FullPizza" */ './pages/FullPizza'))
+const NotFound = lazy(() => import( /* webpackChunkName: "NotFound" */ './pages/NotFound'))
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<MainLayout/>}>
+            <Route path="/" element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/pizza/:id" element={<FullPizza />} />
+                <Route path="/cart" element={<Suspense fallback={<div>Идёт загрузка корзины...</div>}><Cart /></Suspense>} />
+                <Route path="/pizza/:id" element={<Suspense fallback={<div>Идёт загрузка...</div>}><FullPizza /></Suspense>} />
                 <Route path="/react-pizza-v2" element={<Navigate to={"/"} />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="*" element={<Suspense fallback={<div>Идёт загрузка...</div>}><NotFound /></Suspense>} />
             </Route>
         </Routes>
     );
