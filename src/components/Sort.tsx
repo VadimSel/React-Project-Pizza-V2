f396c02e8fb032d36b171d38ed1ab8f570683a52
VadimSel <selyaaa33@gmail.com>
2024-09-11 12:38:13 +0300

    add arrows

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -18,12 +18,12 @@ type SortPopupProps = {
 }
 
 export const sortList: SortItem[] = [
-    { name: "популярности (DESC)", sortProperty: SortPropertyEnum.RATING_DESC },
-    { name: "популярности (ASC)", sortProperty: SortPropertyEnum.RATING_ASC },
-    { name: "цене (DESC)", sortProperty: SortPropertyEnum.PRICE_DESC },
-    { name: "цене (ASC)", sortProperty: SortPropertyEnum.PRICE_ASC },
-    { name: "алфавиту (DESC)", sortProperty: SortPropertyEnum.TITLE_DESC },
-    { name: "алфавиту (ASC)", sortProperty: SortPropertyEnum.PRICE_ASC },
+    { name: "популярности ↓", sortProperty: SortPropertyEnum.RATING_DESC },
+    { name: "популярности ↑", sortProperty: SortPropertyEnum.RATING_ASC },
+    { name: "цене ↓", sortProperty: SortPropertyEnum.PRICE_DESC },
+    { name: "цене ↑", sortProperty: SortPropertyEnum.PRICE_ASC },
+    { name: "алфавиту ↓", sortProperty: SortPropertyEnum.TITLE_DESC },
+    { name: "алфавиту ↑", sortProperty: SortPropertyEnum.PRICE_ASC },
 ];
 
 export const SortPopup: React.FC<SortPopupProps> = memo(({ value }) => {
